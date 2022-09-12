0591963f4fcf6be0c61436174ba90c2e20d81802
Yoari <yoari.gomez06@gmail.com>
2022-09-12 17:52:32 -0500

    tipos de datos primera hora

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
 
+// objetos 
 
+let amaterno:string | number;
+
+amaterno='gonzález';
+amaterno=100;
+
+let mascotas=['perro','gato',100,true];
+
+let tem2:(boolean|number|string)[]=['rata',100];
+
+tem2.push(200);
+
+let 
--- src/repaso/01-tipos-datos.ts
+++ src/repaso/01-tipos-datos.ts
@@ -4,7 +4,7 @@ console.log('Hola Mundo!');
 
 let nombre : string = "Yoari";
 let cont : number =111;
-let acredita : boolem=false;
+let acredita : boolean=false;
 
 let apaterno : number | string ='juan';
 
