9240c35e6671ec876e4e5b04f688ddda36f09ccd
Yoari <yoari.gomez06@gmail.com>
2022-09-12 14:05:03 -0500

    01-tipos-datos

---
+++ src/repaso/01-tipos-datos.ts
@@ -0,0 +1,15 @@
+
+
+console.log('Hola Mundo!');
+
+let nombre : string = "Yoari";
+let cont : number =111;
+let acredita : boolem=false;
+
+let apaterno : number | string ='juan';
+
+apaterno = 238;
+
+var miVariable : string = 'Hola';
+
+const tem : number = 234;
