5559d71a811928fb05b536a1b426e62f26138a3c
Yoari <yoari.gomez06@gmail.com>
2022-09-12 20:54:54 -0500

    Practica 2-02_obj_interface-03_finciones

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,27 @@
+//Practica1
 
+interface utlAlumno{
+    nombre:string;
+    edad:number;
+    direccion:{
+        calle:string;
+        pais:string;
+        estado:string;
+        numero:number;
+    }
+}
+
+const pruebaAlumno:utlAlumno={
+    nombre:'Yoari',
+    edad:21,
+    direccion:{
+        calle:'Lambda',
+        pais:'Mexico',
+        estado:'Guanajuato',
+        numero:511,
+    }
+    function mostrarDireccion(nombres:nombre ,direccion.estado:direccion.estado,direccion.pais:direccion.pais): void{
+        return this.nombres+ ', '+this.direccion.estado+', '+this.direccion.pais
+    }
+}
 
