1365ea0faf455a0b8f9acc1a1bfc003f3cad8234
Yoari <yoari.gomez06@gmail.com>
2022-09-15 17:48:12 -0500

    Practica-01 Corregida

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,22 @@
-//Practica1
 
-interface utlAlumno{
-    nombre:string;
-    edad:number;
-    direccion:{
-        calle:string;
-        pais:string;
-        estado:string;
-        numero:number;
-    }
+interface Detalles{
+    autor:string;
+    anio:number;
 }
 
-const pruebaAlumno:utlAlumno={
-    nombre:'Yoari',
-    edad:21,
-    direccion:{
-        calle:'Lambda',
-        pais:'Mexico',
-        estado:'Guanajuato',
-        numero:511,
-    }
-    function mostrarDireccion(nombres:nombre ,direccion.estado:direccion.estado,direccion.pais:direccion.pais): void{
-        return this.nombres+ ', '+this.direccion.estado+', '+this.direccion.pais
-    }
+interface reproducir{
+    volumen:number;
+    segundo:number;
+    cancion:string;
+    detalles:Detalles
 }
 
+const reproductor:Reproductor={
+    volumen:90,
+    segundo:60,
+    cancion:'Mañanitas'
+    detalles:{
+        autor:'Cepillin',
+        anio:1900,
+    }
+}
---
+++ src/repaso/Practica-01.ts
@@ -0,0 +1,32 @@
+//Practica1
+
+/*interface utlAlumno{
+    nombre:string;
+    edad:number;
+    direccion: Direccion;
+    mostrarDireccion;()
+    }
+
+interface Direccion{
+    calle:string;
+    pais:string;
+    estado:string;
+    numero:number;
+}
+
+const pruebaAlumno:utlAlumno={
+    nombre:'Yoari',
+    edad:21,
+    direccion:{
+        calle:'Lambda',
+        pais:'Mexico',
+        estado:'Guanajuato',
+        numero:511,
+    }
+    mostrarDireccion(){
+        return this.nombres+ ', '+this.direccion.estado+', '+this.direccion.pais;
+    }
+}
+const direccion=alumnoUTL.mostrarDireccion();
+console.log(direccion);
+*/
